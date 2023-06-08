f4942c1032a7649a91df613e8c86fa0feb071c5c
tarynries <taryn.ries@gmail.com>
2023-06-08 20:50:20 +0200

    set up homepage

---
+++ frontend/src/homepage/Homepage.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./Homepage.css";
+
+
+/** Homepage of site.
+ */
+
+function Homepage() {
+    console.debug("Homepage", "currentUser=", currentUser);
+
+    return (
+        <div className="Homepage">
+            <div className="container text-center">
+                <h1 className="mb-4 font-weight-bold">Evolution Meal Planning</h1>
+                <p className="lead">All the recipes and meal planning in one, convenient place.</p>
+                {currentUser
+                    ? <h2>
+                        Welcome Back, {currentUser.username}!
+                    </h2>
+                    : (
+                        <p>
+                            <Link className="btn btn-primary font-weight-bold mr-3"
+                                to="/recipes">
+                                Recipes
+                            </Link>
+                            <br></br>
+                            <Link className="btn btn-primary font-weight-bold"
+                                to="/shopping">
+                                Shopping List
+                            </Link>
+                        </p>
+                    )}
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
