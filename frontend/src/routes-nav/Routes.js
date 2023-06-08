3ba93096a8e8f36229cebac7680d0a2558791f82
tarynries <taryn.ries@gmail.com>
2023-06-08 20:45:53 +0200

    routes file

---
+++ frontend/src/routes-nav/Routes.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Redirect, useNavigate } from "react-router-dom";
+import Homepage from "../homepage/Homepage";
+
+/** Site-wide routes.
+ *
+ */
+
+function AppRoutes() {
+    return (
+
+        // need to edit this piece 
+        <div className="pt-5">
+
+            <Routes>
+
+                <Route path="*" element={<Homepage />} ></Route>
+
+                <Route path="*" element={() => {
+                    navigate("/");
+                    return null;
+                }} ></Route>
+            </Routes>
+
+
+
+        </div>
+    );
+}
+
+export default AppRoutes;
