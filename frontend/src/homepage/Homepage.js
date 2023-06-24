32caf0a3c10a34c099e23389a1359fadf8f8b6ce
tarynries <taryn.ries@gmail.com>
2023-06-24 22:48:46 +0200

    homepage test passing

--- frontend/src/homepage/Homepage.js
+++ frontend/src/homepage/Homepage.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import './Homepage.css'
+import "./Homepage.css";
 
 
 /** Homepage of site.
@@ -12,31 +12,22 @@ function Homepage() {
         <div className="Homepage">
             <div className="container text-center">
                 <h1 className="mb-4 font-weight-bold">Evolution Recipe Planning</h1>
-                <p className="lead">Recipes and shopping list planning in one convenient place.</p>
+                <div className="lead">Recipes and shopping list planning in one convenient place.</div>
 
-                <p>
-                    <div className="btn-container">
-                        <Link className="btn btn-primary font-weight-bold mr-3"
-                            to="/recipes">
-                            Recipes
-                        </Link>
-                        <br></br>
-                        <Link className="btn btn-primary font-weight-bold mr-3"
-                            to="/shopping">
-                            Shopping List
-                        </Link>
-                        <br></br>
-                        <Link className="btn btn-primary font-weight-bold mr-3"
-                            to="/jokes">
-                            Jokes
-                        </Link>
-                    </div>
-                    {/* <br></br>
-                    <Link className="btn btn-primary font-weight-bold"
+                <div className="btn-container">
+                    <Link className="btn btn-primary font-weight-bold mr-3"
+                        to="/recipes">
+                        Recipes
+                    </Link>
+                    <Link className="btn btn-primary font-weight-bold mr-3"
                         to="/shopping">
                         Shopping List
-                    </Link> */}
-                </p>
+                    </Link>
+                    <Link className="btn btn-primary font-weight-bold mr-3"
+                        to="/jokes">
+                        Jokes
+                    </Link>
+                </div>
 
             </div>
         </div>
